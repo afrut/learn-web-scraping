4bf74b49cd53ed2a68317af3ed124aebb3f4bdbf
eric373 <25942990+eric373@users.noreply.github.com>
2023-12-07 19:19:26 -0500

    Add environment

--- indeed/main.py
+++ indeed/main.py
@@ -1,78 +1,93 @@
-# exec(open(".\\indeed\\main.py").read())
+# exec(open("./indeed/main.py").read())
 # A script to scrape basic job information from Indeed
-import subprocess as sp
-import requests
+import hashlib
 import random
+import subprocess as sp
+import time
+
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import hashlib
-import time
-import random
+from selenium.webdriver.chrome.service import Service
 
-dirpath = ".\\indeed\\"
+dirpath = "./indeed/"
+
 
 def getSoup(url: str, driver) -> BeautifulSoup:
     driver.get(url)
     return BeautifulSoup(driver.page_source, "html.parser")
 
+
 def cookSoup(soup: BeautifulSoup) -> list:
     # id = "resultscol" is the primary parent that contains all job data and
     # pagination links
-    results = soup.find(id = "resultsCol")
+    results = soup.find(id="resultsCol")
 
     # id = "mosaic-provider-jobcards" is the parent of <ul
-    # class="jobsearch-resultsList"> that contains the list of jobs. 
-    joblist = list(soup.find(id = "mosaic-provider-jobcards")\
-        .find("ul", class_ = "jobsearch-ResultsList")\
-        .findChildren("li", recursive = False))
+    # class="jobsearch-resultsList"> that contains the list of jobs.
+    joblist = list(
+        soup.find(id="mosaic-provider-jobcards")
+        .find("ul", class_="jobsearch-ResultsList")
+        .findChildren("li", recursive=False)
+    )
 
     # Extract job data.
     ret = dict()
     for job in joblist:
-        jobdata = job.find("table", class_ = "jobCard_mainContent big6_visualChanges")
+        jobdata = job.find("table", class_="jobCard_mainContent big6_visualChanges")
         if jobdata is None:
             continue
-        resultContent = jobdata.find("td", class_ = "resultContent")
-        jobTitleTag = resultContent.find("h2", class_ = "jobTitle")
+        resultContent = jobdata.find("td", class_="resultContent")
+        jobTitleTag = resultContent.find("h2", class_="jobTitle")
         jobTitle = jobTitleTag.text
-        link = "https://ca.indeed.com" +\
-            jobTitleTag.findChildren("a", recursive = "false")[0]["href"]
+        link = (
+            "https://ca.indeed.com"
+            + jobTitleTag.findChildren("a", recursive="false")[0]["href"]
+        )
         if jobTitle[0:3] == "new":
             jobTitle = jobTitle[3:]
-        companyName = resultContent.find("span", class_ = "companyName").text
-        companyLocation = resultContent.find("div", class_ = "companyLocation").text
+        companyName = resultContent.find("span", class_="companyName").text
+        companyLocation = resultContent.find("div", class_="companyLocation").text
         ls = [jobTitle, companyName, companyLocation, link]
         m = hashlib.sha256()
         m.update(b"".join(list(map(lambda x: x.encode("utf-8"), ls))))
         hash = m.hexdigest()
-        ret[hash] = {"jobTitle": jobTitle, "companyName": companyName
-            ,"companyLocation": companyLocation, "link": link, "hash": hash}
+        ret[hash] = {
+            "jobTitle": jobTitle,
+            "companyName": companyName,
+            "companyLocation": companyLocation,
+            "link": link,
+            "hash": hash,
+        }
     return ret
 
-if __name__ == "__main__":
-    sp.call("cls", shell = True)
 
-    chromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-    chromeDriverPath = "D:\\scraping\\driver\\chromedriver.exe"
+if __name__ == "__main__":
+    chromePath = "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe"
+    chromeDriverPath = "/mnt/d/scraping/driver/chromedriver.exe"
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
     options.binary_location = chromePath
     jobs = dict()
     hashKeys = set()
-    with webdriver.Chrome(executable_path = chromeDriverPath, chrome_options = options) as driver:
+    service = Service(chromeDriverPath)
+    with webdriver.Chrome(service=service, options=options) as driver:
         for start in range(740, 800, 10):
-            url = "https://ca.indeed.com/jobs?q=developer&l=Canada&sort=date&filter=0" + f"&start={start}"
+            url = (
+                "https://ca.indeed.com/jobs?q=developer&l=Canada&sort=date&filter=0"
+                + f"&start={start}"
+            )
             soup = getSoup(url, driver)
             cooked = cookSoup(soup)
 
             # Get unique entries
             keys = set(cooked.keys())
-            if not(keys.issubset(hashKeys)):
+            if not (keys.issubset(hashKeys)):
                 hashKeys.update(keys)
                 jobs.update(cooked)
-                ts = 1 + random.randint(1,3) + random.random()
+                ts = 1 + random.randint(1, 3) + random.random()
                 print(f"Scraped start = {start}. Sleeping for {ts}s")
                 time.sleep(ts)
             else:
